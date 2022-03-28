3087c0144a31289b7ba761ab2f2e751514e466a0
Mike Smith <grimbough@gmail.com>
2022-03-28 15:22:23 +0200

    Update footnote matching regex

--- R/modified_tufte.R
+++ R/modified_tufte.R
@@ -1,36 +1,19 @@
-# taken from Tufte version 0.5.1
-parse_footnotes = function(x, fn_label = 'fn') {
-    i = which(x == '<div class="footnotes">')
-    if (length(i) == 0) return(list(items = character(), range = integer()))
-    j = which(x == '</div>')
-    j = min(j[j > i])
-    n = length(x)
-    r = sprintf(
+# taken from Tufte version 0.12.0
+parse_footnotes <- function(x, fn_label = "fn") {
+    i <- grep('^<div class="footnotes[^"]*"[^>]*>', x)
+    if (length(i) == 0) {
+        return(list(items = character(), range = integer()))
+    }
+    j <- which(x == "</div>")
+    j <- min(j[j > i])
+    #n <- length(x)
+    r <- sprintf(
         '<li id="%s([0-9]+)"><p>(.+)<a href="#%sref\\1"([^>]*)>.{1,2}</a></p></li>',
         fn_label, fn_label
     )
-    s = paste(x[i:j], collapse = '\n')
+    s <- paste(x[i:j], collapse = "\n")
     list(
-        items = gsub(r, '\\2', unlist(regmatches(s, gregexpr(r, s)))),
+        items = gsub(r, "\\2", unlist(regmatches(s, gregexpr(r, s)))),
         range = i:j
     )
 }
-
-# parse_footnotes <- function(x, fn_label = "fn") {
-#     i <- grep('^<div class="footnotes[^"]*"[^>]*>', x)
-#     if (length(i) == 0) {
-#         return(list(items = character(), range = integer()))
-#     }
-#     j <- which(x == "</div>")
-#     j <- min(j[j > i])
-#     n <- length(x)
-#     r <- sprintf(
-#         '<li id="%s([0-9]+)"><p>(.+)<a href="#%sref\\1"([^>]*)>.{1,2}</a></p></li>',
-#         fn_label, fn_label
-#     )
-#     s <- paste(x[i:j], collapse = "\n")
-#     list(
-#         items = gsub(r, "\\2", unlist(regmatches(s, gregexpr(r, s)))),
-#         range = i:j
-#     )
-# }
